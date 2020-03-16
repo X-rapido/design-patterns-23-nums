bf64581d5dae0f0c74b784eee736500143c929d5
听风 <tingfeng@helijia.com>
2020-03-16 17:56:23 +0800

    新增案例

--- src/main/java/com/example/实例2/MainClass.java
+++ src/main/java/com/example/实例2/MainClass.java
@@ -8,12 +8,25 @@ import com.example.实例2.model.B_Resp;
 import com.example.实例2.service.impl.ItemIntegratedServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * 优势：
+ *      1、结构清晰，请求/响应模块化
+ *      2、具有顺序请求，B_Req 请求条件，可以依赖上层 A_Resp 结果
+ *      3、ItemIntegratedServiceImpl 各个业务模块方法可以互相调用
+ * 缺点：
+ *      1、每个请求/响应都需要模块化处理
+ *      2、每个模块，需增量定义 ItemIntegratedServiceImpl 方法
+ * 思考：
+ *      1、全局处理性能问题
+ */
 public class MainClass {
 
     @Autowired
     private ItemIntegratedServiceImpl itemIntegratedService;
 
     public void updateItem (String itemId) {
+        // 1、
+
         A_Resp a_resp = itemIntegratedService.methodA(new A_Req());
 
         B_Resp b_resp = itemIntegratedService.methodB(new B_Req());
